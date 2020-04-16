72593cbdf77b8b3ffd2f5843506efe6a81d1a19e
ilham <ilhamfathys@gmail.com>
2020-04-16 20:11:21 +0700

    Create api utils and complete disburse api using given 3rd API.

--- mymarket/api/disbursement/disburse.php
+++ mymarket/api/disbursement/disburse.php
@@ -1,10 +1,10 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Max-Age: 3600');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 // get database connection
 include_once '../../config/db.php';
@@ -12,29 +12,18 @@ include_once '../../config/db.php';
 // instantiate Disbursement object
 include_once '../../objects/disbursement.php';
 
+include_once '../utils/api_utils.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
 $disbursement = new Disbursement($db);
 
-$request = json_decode(file_get_contents("php://input"));
+$request = json_decode(file_get_contents('php://input'));
+$response = json_decode(call_api('POST', 'https://nextar.flip.id/disburse',
+                                 array('bank_code' => $request->bank_code, 'account_number' => $request->account_number,
+                                 'amount' => $request->amount, 'remark' => $request->remark)));
 
-$response = new StdClass();
-// Mock
-$response->id = 55351525642;
-$response->amount = 10000;
-$response->status = "PENDING";
-$response->timestamp = "2019-05-21 09:12:42";
-$response->remark = "sample remark";
-$response->bank_code = "bni";
-$response->account_number = "1234567890";
-$response->beneficiary_name = "PT FLIP";
-$response->receipt = null;
-$response->time_served = "0000-00-00 00:00:00";
-$response->fee = 4000;
-
-// set product property values
 $disbursement->id = $response->id;
 $disbursement->amount = $response->amount;
 $disbursement->status = $response->status;
@@ -54,7 +43,7 @@ if($disbursement->create()){
     http_response_code(201);
 
     // tell the user
-    echo json_encode(array("message" => "Disbursement was created.", "disbursement" => $disbursement));
+    echo json_encode(array('message' => 'Disbursement was created.', 'disbursement' => $disbursement));
 }
 
 // if unable to create the Disbursement, tell the user
@@ -64,7 +53,7 @@ else{
     http_response_code(503);
 
     // tell the user
-    echo json_encode(array("message" => "Unable to create disbursement."));
+    echo json_encode(array('message' => 'Unable to create disbursement.'));
 }
 
 ?>
